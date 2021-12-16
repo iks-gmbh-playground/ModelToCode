4d425df0d622b2cc6a28c33142da06c12b99fe99
Jan <jp.radeck@gmx.de>
2021-12-16 11:23:05 +0100

    Stand gesichert

---
+++ Flexml.Parser.Ch3/src/main/java/flexml/parser/app/XmlParserApp.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package flexml.parser.app;
+
+import java.io.File;
+
+import flexml.elements.FileElement;
+import flexml.parser.XmlParser;
+
+/**
+ * 
+ *
+ */
+public class XmlParserApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		File folder = new File(args[0]);
+		
+		 for (final File fileEntry : folder.listFiles()) {
+		        if (fileEntry.isFile()) {
+		        	System.out.println(fileEntry.getName());
+		        	XmlParser parser = new XmlParser(fileEntry.getAbsolutePath());
+		        	FileElement fe = parser.parse();
+		        	System.out.println(fe.toString());
+		        } else {
+		            
+		        }
+		    }		
+		
+
+	}
+
+}
